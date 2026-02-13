491071cab9944efe015f9368c6db2a7752404ae4
Eduardo Valencia <EduardoVG@proton.me>
2026-02-13 00:38:32 -0600

    Enhance Index page layout and styling

--- IS262E15Ejercicio04/src/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ IS262E15Ejercicio04/src/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+139f8e32533ba3be4c5f7c4d061eb784d531c034")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
